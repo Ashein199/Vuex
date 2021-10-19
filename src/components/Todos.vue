bdc3c39fd395f5dfc4e25bff9874b54cf918334d
Ashein199 <heroglasses155@gmail.com>
2021-10-19 13:07:19 +0630

    show todos from api

---
+++ src/components/Todos.vue
@@ -0,0 +1,28 @@
+<template>
+  <div>
+    <h3 class="text-primary text-center">All Todos</h3>
+    <div class="container">
+      <div class="row">
+        <div class="col-md-4 my-4" v-for="todo in myTodos" :key="todo.id">
+          <b-card bg-variant="primary" text-variant="white" class="text-center">
+            <b-card-text>{{ todo.title }}</b-card-text>
+          </b-card>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+import { mapGetters, mapActions } from "vuex";
+export default {
+  computed: mapGetters(["myTodos"]),
+  methods: mapActions(["getTodos"]),
+  mounted() {
+    console.log(this.getTodos());
+  },
+};
+</script>
+
+<style>
+</style>
